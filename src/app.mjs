0bc7e79267bd57099ae5301a7bc756c87d15c303
saurabh <sauravvermas1097v@gmail.com>
2023-07-27 17:14:35 +0530

    create main app file

---
+++ src/app.mjs
@@ -0,0 +1,56 @@
+// app.js
+import express from "express";
+import { Double, MongoClient, ServerApiVersion } from "mongodb";
+import { connectDB } from "./config/index.mjs"
+import dotenv from 'dotenv';
+import authRoutes from "./routes/authRoutes.mjs"
+import userRoutes from "./routes/userRoutes.mjs"
+import dealershipRoutes from "./routes/dealershipRoutes.mjs"
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+const db = await connectDB();
+// Attach database object to the route handlers
+app.use((req, res, next) => {
+    req.database = db;
+    next();
+});
+
+app.use(express.json());
+
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Dealership App',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./src/routes/JSDoc.mjs'], // files containing annotations as above
+};
+  const swaggerSpec = swaggerJsDoc(options);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  
+
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+app.use('/dealership', dealershipRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
