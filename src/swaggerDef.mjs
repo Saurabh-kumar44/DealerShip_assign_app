84514eb035c5aba057f2fd7d7f5eb7df02883945
saurabh <sauravvermas1097v@gmail.com>
2023-07-27 17:19:39 +0530

    creating documentation

---
+++ src/swaggerDef.mjs
@@ -0,0 +1,34 @@
+// src/swaggerDef.js
+
+import swaggerJSDoc from "swagger-jsdoc";
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: API endpoints for managing users
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Dealership
+ *   description: API endpoints for managing dealerships
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: API endpoints for authentication
+ */
